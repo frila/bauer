1b98dbfd73886466950caff26e5ab446f09741db
Bernardo Lins <bernardolins28@gmail.com>
2015-07-30 10:27:01 -0300

    Criando classe bauer_node

---
+++ src/bauer_node.hpp
@@ -0,0 +1,50 @@
+#ifndef BAUER_NODE_HPP
+#define BAUER_NODE_HPP
+
+#include <iostream>
+#include "bauer_types.hpp"
+
+namespace bauer {
+  class bauer_node
+  {
+  private:
+    bsocket_t socket;
+    //TODO: transformar bauer_ip em classe, atualmente é string declarada em bauer_types.hpp
+    bauer_ip ip;
+    bauer_port port;
+  public:
+    bauer_node(bsocket_t _socket, bauer_ip _ip, bauer_port _port) {
+      this->socket = _socket;
+      this->ip = _ip;
+      this->port = _port;
+    }
+
+    bsocket_t get_socket() {
+      return socket;
+    }
+
+    bauer_ip get_ip() {
+      return ip;
+    }
+
+    bauer_port get_port() {
+      return port;
+    }
+
+    void set_socket(bsocket_t val) {
+      socket = val;
+    }
+
+    void set_ip(bauer_ip val) {
+      ip = val;
+    }
+
+    void set_port(bauer_port val) {
+      port = val;
+    }
+
+    ~bauer_node() {}
+  };
+}
+
+#endif
---
+++ src/bauer_types.hpp
@@ -0,0 +1,7 @@
+#ifndef BAUER_TYPES_HPP
+#define BAUER_TYPES_HPP
+
+typedef int bsocket_t;
+typedef string bauer_ip;
+typedef short bauer_port;
+#endif
