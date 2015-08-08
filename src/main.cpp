865bc9ed07d85969daeaad1a120d73ad28fa441b
Fausto Junqueira <faustoferjunqueira@gmail.com>
2015-08-08 01:01:48 -0300

    implement exception and add in main

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,6 @@ int main(int argc, char const *argv[])
 {
   throw bauer_socket_exception();
 }
+
+
+//docker run -d --name="drone" -p 8081:8081 -v /var/lib/drone/ -v /var/run/docker.sock:/var/run/docker.sock -v /opt/docker/droneio/drone.sqlite:/var/lib/drone/drone.sqlite -v /opt/docker/droneio:/var/lib/drone faustofjunqueira/drone /usr/local/bin/droned --config=/var/lib/drone/drone.toml
