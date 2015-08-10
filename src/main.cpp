165781d8a68b148e600fe0eaae17b087e960bb00
Fausto Junqueira <faustoferjunqueira@gmail.com>
2015-08-10 06:46:19 -0300

    Test .drone.yml

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,3 @@ int main(int argc, char const *argv[])
 {
   throw bauer_socket_exception();
 }
-
-
-//docker run -d --name="drone" -p 8081:8081 -v /var/lib/drone/ -v /var/run/docker.sock:/var/run/docker.sock -v /opt/docker/droneio/drone.sqlite:/var/lib/drone/drone.sqlite -v /opt/docker/droneio:/var/lib/drone faustofjunqueira/drone /usr/local/bin/droned --config=/var/lib/drone/drone.toml
